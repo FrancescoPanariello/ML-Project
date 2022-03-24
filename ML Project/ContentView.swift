bdc9d50de2a8c39d1a074c9273dae256e68724d6
Francesco Panariello <72973754+FrancescoPanariello@users.noreply.github.com>
2022-03-24 19:16:38 +0100

    Initial Commit

---
+++ ML Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ML Project
+//
+//  Created by Francesco Panariello on 24/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ML Project/ML_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_ProjectApp.swift
+//  ML Project
+//
+//  Created by Francesco Panariello on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ML_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
