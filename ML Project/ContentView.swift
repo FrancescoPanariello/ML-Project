3541930b04185b40812be3043dcb0fcbbc278794
Francesco Panariello <72973754+FrancescoPanariello@users.noreply.github.com>
2022-04-06 16:57:28 +0200

    6 Apr

--- ML Project/ContentView.swift
+++ ML Project/ContentView.swift
@@ -6,11 +6,68 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
+    let animals = ["tiger","flamingo", "horse"]
+    @State private var num_element : Int = 0
+    let model = SqueezeNet()
+    @State private var classification: String = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Animal Classifier").font(.largeTitle).bold()
+            Image(animals[num_element])
+                .resizable() .scaledToFit()
+            HStack{
+                Button("<"){
+                if(self.num_element != 0){
+                   self.num_element = self.num_element - 1
+                    self.classification = ""
+                }}
+                Spacer()
+                Button("Verify") {
+                    verify()
+                }.foregroundColor(.green)
+                Spacer()
+                Button(">"){
+                if(self.num_element < animals.count - 1){
+                   self.num_element = self.num_element + 1
+                    self.classification = ""
+                }}
+            }.padding()
+        
+            Divider()
+            VStack(spacing: 20){
+        Text("Result:")
+        Text(classification)
+            }
+            
+        Spacer()
+        }
+    }
+    
+    func verify(){
+        let currentImageName = animals[num_element]
+        
+        guard let image = UIImage(named: currentImageName),
+              let resizedImage = image.converterpixelBuffer(width: 227, height: 227) else{
+                  return
+              }
+             
+        
+        let output = try? model.prediction(image: resizedImage)
+        if let output = output {
+                let results = output.classLabelProbs.sorted { $0.1 > $1.1 }
+            
+            let top3 = results.prefix(3).map { (key, value) in
+                    return "\(key) = \(String(format: "%.2f", value * 100))%"
+                }.joined(separator: " \n")
+
+               
+                self.classification = top3
+            }
     }
 }
 
